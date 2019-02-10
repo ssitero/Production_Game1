be396080b33231459aa56536ceb3792a73e4157e
Samuel <ssitero@knights.ucf.edu>
2019-02-09 20:58:36 -0500

    patrol starting

---
+++ Production_Game1/Assets/Scripts/Patrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour {
+
+    private NavMeshAgent navAgent;
+    private float patrol_Radius = 30f;
+    private float patrol_Timer = 10f;
+    private float timer_Count;
+
+    void Awake (){
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        timer_Count = patrol_Timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Patroling();
+	}
+
+    void Patroling(){
+        timer_Count += Time.deltaTime;
+
+        if (timer_Count > patrol_Timer){
+            SetNewRandomDestination();
+            timer_Count = 0f;
+        }
+
+    }
+    void SetNewRandomDestination(){
+
+    }
+}
+
