c00dd7323df5fc7a714c499f2bc6d79676194cda
Samuel <ssitero@knights.ucf.edu>
2019-02-10 01:40:51 -0500

    patrol almost done

--- Production_Game1/Assets/Scripts/Patrol.cs
+++ Production_Game1/Assets/Scripts/Patrol.cs
@@ -7,7 +7,7 @@ public class Patrol : MonoBehaviour {
 
     private NavMeshAgent navAgent;
     private float patrol_Radius = 30f;
-    private float patrol_Timer = 10f;
+    private float patrol_Timer = 6f;
     private float timer_Count;
 
     void Awake (){
@@ -35,6 +35,17 @@ public class Patrol : MonoBehaviour {
     }
     void SetNewRandomDestination(){
 
+        Vector3 newDestination = RandomNavSphere(transform.position, patrol_Radius, -1);
+        navAgent.SetDestination(newDestination);
+    }
+    Vector3 RandomNavSphere(Vector3 originPos, float radius, int layerMask){
+        Vector3 randDir = Random.insideUnitSphere * radius;
+        randDir += originPos;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDir, out navHit, radius, layerMask);
+
+        return navHit.position;
     }
 }
 
