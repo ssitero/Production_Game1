59fdb07cea66f3433955e0539c3eb105abf8fc90
Samuel <ssitero@knights.ucf.edu>
2019-02-13 16:26:40 -0500

    working on enemy freeze

--- Production_Game1/Assets/Scripts/BulletScript.cs
+++ Production_Game1/Assets/Scripts/BulletScript.cs
@@ -2,24 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour {
+public class BulletScript : MonoBehaviour
+{
 
     public float speed = 8f;
     public float lifetime = 2f;
+   // public GameObject enemy;
 
     private float lifeTimer;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         lifeTimer = lifetime;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         transform.position += transform.forward * speed * Time.deltaTime;
         lifeTimer -= Time.deltaTime;
-        if (lifeTimer <= 0f){
+        if (lifeTimer <= 0f)
+        {
             Destroy(gameObject);
         }
     }
+    /*
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Guard"))
+        {
+
+            enemy.GetComponent("Guard").enabled = false;
+        }
+    }
+    */
 }
